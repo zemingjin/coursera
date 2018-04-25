09716299857ed964886fc7660b7b709b1d845dee
Ming Jin <zejin@expedia.com>
2018-04-24 19:04:59 -0500

    Lecture 4.1

---
+++ src/main/scala/coursera/course4/Nat.scala
@@ -0,0 +1,27 @@
+package coursera.course4
+
+abstract class Nat {
+  def isZero: Boolean
+  def predecessor: Nat
+  def successor: Nat = Succ(this)
+  def + (that: Nat): Nat
+  def - (that: Nat): Nat
+}
+
+object Zero extends Nat {
+  override def isZero: Boolean = true
+  override def predecessor: Nat = throw new Error("0.predecessor")
+  override def +(that: Nat): Nat = that
+  override def -(that: Nat): Nat =
+    if (that.isZero) this
+    else throw new Error("negative number")
+}
+
+case class Succ(n: Nat) extends Nat {
+  override def isZero: Boolean = false
+  override def predecessor: Nat = n
+  override def +(that: Nat): Nat = Succ(n + that)
+  override def -(that: Nat): Nat =
+    if (that.isZero) this
+    else n - that.predecessor
+}
